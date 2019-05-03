48f1ab8a5ed02b2bcfb4b469d6b897c0a547fca5
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-05-02 23:30:12 -0700

    bug

--- pro_gan_pytorch/CustomLayers.py
+++ pro_gan_pytorch/CustomLayers.py
@@ -420,7 +420,7 @@ class ConDisFinalBlock(th.nn.Module):
 
         # we also need an embedding matrix for the label vectors
         self.label_embedder = Embedding(num_classes, in_channels)
-        norms = torch.norm(self.label_embedder.weight, p=2, dim=1).data
+        norms = th.norm(self.label_embedder.weight, p=2, dim=1).data
         self.label_embedder.weight.data = self.label_embedder.weight.data.div(norms.view(num_classes,1).
                                                                               expand_as(self.label_embedder.weight))
         # leaky_relu:
